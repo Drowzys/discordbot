92dc085de2b20c75f1d071b896789574722d690f
Rodrigo Munoz <rm117579@gmail.com>
2017-08-12 02:13:21 -0500

    added disconnect and disconnect

--- bot1.py
+++ bot1.py
@@ -18,4 +18,22 @@ async def ping():
 	await client.say("Pong!")
 
 
+@client.command(pass_context= True)
+async def connect(ctx)):
+	if client.is_voice_connected(ctx, message.server):
+		return await client.say("I'm already connected!")
+	author = ctx.mesasge.author
+	voice_channel = author.voice_channel
+	vc = await client.join_voice_channel(voice_channel)
+
+@client.comman(pass_context= True)
+async def disconnect(ctx):
+	for x in client.voice_clients:
+		if(x.server == ctx.mesasge.server)
+			return await x.disconnect()	
+
+
+
+
+
 client.run('MzQ0NjE5MTk5OTk1NDQ1MjU5.DHAfKA.o1t935Ij4AkIIDpPNXktrzexQYI')

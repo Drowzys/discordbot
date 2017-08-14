85863e4ee7eb03cde46ec6e84b27ca4103de6eaf
Rodrigo Munoz <rm117579@gmail.com>
2017-08-14 07:57:26 -0500

    Added comments to help understand the code better

--- bot1.py
+++ bot1.py
@@ -14,19 +14,19 @@ client = discord.Client()
 bot_prefix= "."
 client = commands.Bot(command_prefix=bot_prefix)
 
-
+#stuff it prints into the console
 @client.event
 async def on_ready():
     print("Bot Online!")
     print("Name: {}".format(client.user.name))
     print("ID: {}".format(client.user.id))
-
+#test bot online command
 @client.command(pass_context=True)
 async def ping(ctx):
 	await client.say("Pong!")
 	
 
-
+#make bot connect command
 @client.command(pass_context=True)
 async def connect(ctx):
     if client.is_voice_connected(ctx.message.server):
@@ -34,13 +34,13 @@ async def connect(ctx):
     author = ctx.message.author
     voice_channel = author.voice_channel
     vc = await client.join_voice_channel(voice_channel)
-
+#bot is able to disconnect
 @client.command(pass_context = True)
 async def disconnect(ctx):
     for x in client.voice_clients:
         if(x.server == ctx.message.server):
             return await x.disconnect()	
-
+#assigns the person which requested an identity
 @client.command(pass_context=True)
 async def roleplay(ctx):
     name()
@@ -49,7 +49,7 @@ async def roleplay(ctx):
     race()
 
     await client.say("Your role is {}, you are {} years old, you are {} and you are {}!".format(name(), age(), char(), race()))
-
+#clear the chat messages
 @client.command(pass_context=True)       
 async def clear(ctx, number):
     mgs = []
@@ -58,11 +58,11 @@ async def clear(ctx, number):
         mgs.append(x)
     await client.delete_messages(mgs)
 
-
+#you can rape someone
 @client.command(pass_context=True)
 async def rape(ctx, args):
 	await client.say("You raped {}".format(args))
 
     
-
+#runs the bot
 client.run('MzQ0NjE5MTk5OTk1NDQ1MjU5.DHAfKA.o1t935Ij4AkIIDpPNXktrzexQYI')
